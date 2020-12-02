93e6a90d186d65897de2b365ee978fcb90065b4b
nicocastillob <nicolascastillob@gmail.com>
2020-12-02 15:57:27 -0300

    comentarios solicitu.go y com_cliente, se borro cartpeta libros

--- NameNode/ClienteName/solicitud.go
+++ NameNode/ClienteName/solicitud.go
@@ -17,14 +17,17 @@ type Server struct {
 //SolicitudCliente procesa las solucitudes de los clientes
 func (s *Server) SolicitudCliente(ctx context.Context, in *Message) (*Message, error) {
 	mensaje := in.Body
+	// si el mensaje es vacio se están solicitando los titulos disponibles
 	if mensaje == "" {
 		log.Printf("Solicitud titulos disponibles")
 		return &Message{Body: titulos()}, nil
 	}
+	//De lo contrario se ve si existe indormación sobre el titulo solicitado
 	log.Printf("Solicitud ubicaciones de: %s", mensaje)
 	return &Message{Body: ubicaciones(mensaje)}, nil
 }
 
+//función encargada de devolver las ubicaciones de los chunks
 func ubicaciones(libro string) string {
 	var candado sync.Mutex
 	candado.Lock()
@@ -41,6 +44,7 @@ func ubicaciones(libro string) string {
 	return strings.Trim(paraMandar, " ")
 }
 
+//función encargada de devolver los titulos disponibles
 func titulos() string {
 	var retorno string
 	var candado sync.Mutex
