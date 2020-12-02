c42a93319da54e654cc88fc1f85297316dab7f62
nicocastillob <nicolascastillob@gmail.com>
2020-12-02 10:21:37 -0300

    comentarios cliente.go

--- Cliente/cliente.go
+++ Cliente/cliente.go
@@ -20,8 +20,10 @@ var tipo int
 
 func main() {
 
+	//semilla para aleatoriedad
 	rand.Seed(time.Now().UnixNano())
 
+	//Solicitud por pantalla para el comportamiento
 	for {
 		for {
 			fmt.Println("\nIngrese comportamiento a seguir (1/2):\n1 Cliente Uploader\n2 Cliente Downloader")
@@ -31,6 +33,7 @@ func main() {
 			}
 		}
 
+		//Se ejecuta el comportamiento solicitado
 		if tipo == 1 {
 			ClienteUploader()
 		} else {
@@ -40,15 +43,20 @@ func main() {
 
 }
 
+//Comportamiento de cliente uploader
 func ClienteUploader() {
 
+	//libro a enviar
 	var libro string
+	//variables para enviar a un nodo al azar
 	var dir string
 	nodoAzar := rand.Intn(3)
 
+	//se solicita el nombre del libro a subir
 	fmt.Println("\nIngrese nombre de libro a subir (FORMATO: NOMBRE.pdf)")
 	fmt.Scanln(&libro)
 
+	//se abre el archivo
 	file, err := os.Open(libro)
 
 	if err != nil {
@@ -58,8 +66,9 @@ func ClienteUploader() {
 
 	defer file.Close()
 
+	//a partir del valor al azar se define la dirección del nodo
 	if nodoAzar == 0 {
-		dir = "dist46:9001" ////////////////////////////////////////////////////////////////////////////
+		dir = "dist46:9001"
 		fmt.Println(dir)
 	} else if nodoAzar == 1 {
 		dir = "dist47:9001"
@@ -69,6 +78,7 @@ func ClienteUploader() {
 		fmt.Println(dir)
 	}
 
+	//Se establece comunicación
 	var conn *grpc.ClientConn
 	conn, err = grpc.Dial(dir, grpc.WithInsecure())
 	if err != nil {
@@ -76,38 +86,29 @@ func ClienteUploader() {
 	}
 	defer conn.Close()
 
+	//datos necesarios para los chunk
 	fileInfo, _ := file.Stat()
 
 	var fileSize int64 = fileInfo.Size()
 
-	const fileChunk = 250000 // 1 MB, change this to your requirement
-
-	// calculate total number of parts the file will be chunked into
+	const fileChunk = 250000
 
 	totalPartsNum := uint64(math.Ceil(float64(fileSize) / float64(fileChunk)))
 
 	fmt.Printf("Splitting to %d pieces.\n", totalPartsNum)
 
+	//creación y envio de chunks
 	for i := uint64(0); i < totalPartsNum; i++ {
 
+		//creacion chunk
 		partSize := int(math.Min(fileChunk, float64(fileSize-int64(i*fileChunk))))
 		partBuffer := make([]byte, partSize)
 
 		file.Read(partBuffer)
-		/*////////////////////////////////////////////////////////////////////////REEMPLAZAR PARA ENVIAR A DATANODE
-		// write to disk
-		fileName := "bigfile_" + strconv.FormatUint(i, 10)
-		_, err := os.Create(fileName)
 
-		if err != nil {
-			fmt.Println(err)
-			os.Exit(1)
-		}
-
-		// write/save buffer to disk
-		ioutil.WriteFile(fileName, partBuffer, os.ModeAppend)*/
 		c := com_cliente.NewInteraccionesClient(conn)
 
+		//envio de chunk
 		respuesta, err := c.SubirLibro(context.Background(), &com_cliente.Libro{
 			Titulo:      strings.TrimRight(libro, ".pdf"),
 			TotalChunks: totalPartsNum,
@@ -125,8 +126,10 @@ func ClienteUploader() {
 
 }
 
+//comportamiento cliente downloader
 func ClienteDownloader() {
 
+	//se establece comunicación con NameNode
 	fmt.Println("ClienteDownloader")
 	var conn *grpc.ClientConn
 	conn, err := grpc.Dial(":9001", grpc.WithInsecure())
@@ -137,27 +140,30 @@ func ClienteDownloader() {
 
 	c := ClienteName.NewNameServiceClient(conn)
 
-	//solicitar titulos
+	//se solicitan titulos
 	response, err := c.SolicitudCliente(context.Background(), &ClienteName.Message{Body: ""})
 	if err != nil {
 		log.Fatalf("Error al llamar SolicitudCliente: %s", err)
 	}
-
+	//si no hay titulos se termina la ejecucion de cliente downloader
 	if len(response.Body) == 0 {
 		fmt.Println("\nNO HAY LIBROS DISPONIBLES\n")
 		return
 	}
+
+	//se muestran los libros disponibles
 	var titulos []string = strings.Split(response.Body, " ")
 	fmt.Println("\nLIBROS DISPONIBLES\n")
 	for t := 0; t < len(titulos); t++ {
 		fmt.Println(titulos[t])
 	}
 
+	//se solicita el ingreso del libro a descargar
 	var libro string
 	fmt.Println("\nIngrese nombre de libro a solicitar(Igual al mostrado anteriormente, sin espacios)")
 	fmt.Scanln(&libro)
 	libro = strings.TrimSpace(libro)
-	//solicitar ubicaciones
+	//se solicitan ubicaciones de los chunks
 	response, err = c.SolicitudCliente(context.Background(), &ClienteName.Message{Body: libro})
 	if err != nil {
 		log.Fatalf("Error al llamar SolicitudCliente: %s", err)
@@ -169,8 +175,9 @@ func ClienteDownloader() {
 	}
 	var ubicacioneslista []string = strings.Split(ubicaciones, " ")
 	log.Printf("Response from server: %s", ubicacioneslista)
-	///////////////////////////////////COMENZAR A PEDIR CHUNKS
+	//COMENZAR A PEDIR CHUNKS
 
+	//creación del archivo de la descarga
 	newFileName := "Descarga" + libro + ".pdf"
 	_, err = os.Create(newFileName)
 
@@ -179,8 +186,7 @@ func ClienteDownloader() {
 		os.Exit(1)
 	}
 
-	//set the newFileName file to APPEND MODE!!
-	// open files r and w
+	//se abre el archivo de la descarga para comenzar a escribir
 
 	file, err := os.OpenFile(newFileName, os.O_APPEND|os.O_WRONLY, os.ModeAppend)
 
@@ -189,10 +195,10 @@ func ClienteDownloader() {
 		os.Exit(1)
 	}
 
-	//var writePosition int64 = 0
-
+	//Se solicitan los chunks de acurdo a la direccion indicada y se escriben en archivo
 	for d := 0; d < len(ubicacioneslista); d++ {
 
+		//Peticiones de chunks
 		fmt.Println(ubicacioneslista[d])
 		var conn *grpc.ClientConn
 		conn, err := grpc.Dial(ubicacioneslista[d], grpc.WithInsecure())
@@ -211,6 +217,7 @@ func ClienteDownloader() {
 			log.Fatalf("Error al pedir chunk: %s", err)
 		}
 
+		//escritura en archivo
 		if respuesta.Estado {
 			_, err := file.Write(respuesta.Data)
 
