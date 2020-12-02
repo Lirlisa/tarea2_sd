d704f7c54823a594186ce85cc2b5f27e98657470
hcaballero <harold.caballero@sansano.usm.cl>
2020-12-02 20:45:33 -0300

    creada implementación de ricart y walala en namenode

--- NameNode/com_namenode/com_namenode.go
+++ NameNode/com_namenode/com_namenode.go
@@ -9,29 +9,54 @@ import (
 	"../../estructuras"
 )
 
+//ServerNamenode estructura para crear el servidor
 type ServerNamenode struct {
 	placeholder int
 }
 
+//EscribirLog para escribir en el log
 func (c *ServerNamenode) EscribirLog(ctx context.Context, in *Log) (*EstadoEscritura, error) {
 	var candado sync.Mutex
+	candado.Lock()
+	if estructuras.Ocupado {
+		return &EstadoEscritura{
+			Estado: false,
+			Msg:    "toy ocupao",
+		}, nil
+	}
+	estructuras.Ocupado = true
+	candado.Unlock()
 	f, err := os.OpenFile("LOG.txt", os.O_APPEND|os.O_WRONLY|os.O_CREATE, 0600)
 	if err != nil {
 		log.Fatalf("error: ", err.Error())
 	}
 	defer f.Close()
 	candado.Lock()
+	defer candado.Unlock()
 	estructuras.Locaciones[in.GetTitulo()] = in.GetTexto()
-	candado.Unlock()
 	if _, err = f.WriteString(in.GetTexto()); err != nil {
+		estructuras.Ocupado = true
 		return &EstadoEscritura{
 			Estado: false,
 			Msg:    err.Error(),
 		}, nil
-	} else {
-		return &EstadoEscritura{
-			Estado: true,
-			Msg:    "",
+	}
+	estructuras.Ocupado = true
+	return &EstadoEscritura{
+		Estado: true,
+		Msg:    "",
+	}, nil
+
+}
+
+//Request permite saber si se puede escribir en el log
+func (c *ServerNamenode) Request(ctx context.Context, in *Id) (*Disponibilidad, error) {
+	if !estructuras.Ocupado {
+		return &Disponibilidad{
+			Disponible: true,
 		}, nil
 	}
+	return &Disponibilidad{
+		Disponible: false,
+	}, nil
 }
--- estructuras/estructuras.go
+++ estructuras/estructuras.go
@@ -2,3 +2,6 @@ package estructuras
 
 //Locaciones guarda las ubicaciones de los chunks del libro
 var Locaciones map[string]string = make(map[string]string)
+
+//Ocupado para saber si namenode está escribiendo en log
+var Ocupado bool = false
